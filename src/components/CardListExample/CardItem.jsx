69df0969ed63c421bfaa5b838694fc6a3c949d5b
seonmi Kim <seonmi@seonmiui-MacBookPro.local>
2020-06-24 17:02:09 +0900

    style: 불필요한 파일 정리 등

--- src/components/CardListExample/CardItem.jsx
+++ src/components/CardListExample/CardItem.jsx
@@ -3,25 +3,22 @@ import Thumbnail from "../Thumbnail/Thumbnail.jsx";
 import Rating from "../Rating/Rating.jsx";
 import common from "../../helpers/common.js";
 
-const CardItem = ({ title, label, hilight, price, comment, score, thumbnail, writer }) => {
-  console.log("### thumbnail: ", thumbnail);
-  return (
-    <li className="card-item">
-      <Thumbnail url={thumbnail} className="card-item__thumbnail" alt="썸네일 이미지" />
-      <div className="card-item__info">
-        <span className="text-secondary">{label}</span>
-        <em className="text-primary">{title}</em>
-        <strong className="text-primary color-hilight">{common.comma(hilight)}</strong>
-        {price && <del className="text-secondary">{common.comma(price)}</del>}
-      </div>
+const CardItem = ({ title, label, hilight, price, comment, score, thumbnail, writer }) => (
+  <li className="card-item">
+    <Thumbnail url={thumbnail} className="card-item__thumbnail" alt="썸네일 이미지" />
+    <div className="card-item__info">
+      <span className="text-secondary">{label}</span>
+      <em className="text-primary">{title}</em>
+      <strong className="text-primary color-hilight">{common.comma(hilight)}</strong>
+      {price && <del className="text-secondary">{common.comma(price)}</del>}
+    </div>
 
-      <div className="card-item__rating">
-        {score >= 0 && <Rating value={score} />}
-        <em className="text-secondary writer">{writer}</em>
-        {comment && <span className="text-secondary">{comment}</span>}
-      </div>
-    </li>
-  );
-};
+    <div className="card-item__rating">
+      {score >= 0 && <Rating value={score} />}
+      <em className="text-secondary writer">{writer}</em>
+      {comment && <span className="text-secondary">{comment}</span>}
+    </div>
+  </li>
+);
 
 export default CardItem;
--- src/helpers/common.js
+++ src/helpers/common.js
@@ -2,7 +2,7 @@ const commons = {};
 
 /**
  * 3자리 단위마다 콤마
- * @param  {Number} number 변환할 위한 숫자
+ * @param  {Number} number 변환할 숫자
  */
 commons.comma = (number) => {
   const regexp = /\B(?=(\d{3})+(?!\d))/g;
